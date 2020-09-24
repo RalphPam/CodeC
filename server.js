7ef7ef9478043dcce9e7a48eda654a355c4210e0
Ralph Pamintuan <pralphnest@gmail.com>
2020-09-24 21:04:59 +0800

    setup routes

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express')
 const connectDB = require('./config/db')
 
 const app = express()
-
-connectDB()
-
 const PORT = process.env.PORT || 3000
+connectDB()
 
 app.listen(PORT, () => {
    console.log(`Server started at port ${PORT}`)
@@ -14,3 +12,8 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
    res.send('API running')
 })
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
