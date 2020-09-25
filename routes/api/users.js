1f08b98ba7f738a75434bf463587aea0782a25b8
Ralph Pamintuan <pralphnest@gmail.com>
2020-09-25 11:22:12 +0800

    save user data

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/User')
 const { check, validationResult } = require('express-validator')
 
 router.post(
@@ -11,12 +14,40 @@ router.post(
          min: 6,
       }),
    ],
-   (req, res) => {
+   async (req, res) => {
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
          return res.status(400).json({ errors: errors.array() })
       }
-      res.send('User Route')
+
+      const { name, email, password } = req.body
+
+      try {
+         let user = await User.findOne({ email: email })
+         if (user) {
+            return res
+               .status(400)
+               .json({ error: [{ msg: 'User already exists' }] })
+         }
+         const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm',
+         })
+         user = new User({
+            name,
+            email,
+            password,
+            avatar,
+         })
+         const salt = await bcrypt.genSalt(10)
+         user.password = await bcrypt.hash(password, salt)
+         await user.save()
+         res.send('User Route')
+      } catch (error) {
+         console.error(error)
+         res.status(500).send('Server Error')
+      }
    }
 )
 
