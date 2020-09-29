29cf02d0be3f677871db77c2ff73addc26fe52aa
Ralph Pamintuan <pralphnest@gmail.com>
2020-09-29 16:33:53 +0800

    get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -33,12 +33,25 @@ router.post(
    }
 )
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
    try {
-      const allPost = await Post.find()
+      const allPost = await Post.find().sort({ date: -1 })
       res.json(allPost)
    } catch (err) {
       console.error(err.message)
+      res.status(500).send('Server Error')
+   }
+})
+
+router.get('/:id', auth, async (req, res) => {
+   try {
+      const post = await Post.findById(req.params.id)
+      if (!post) return res.status(404).json({ msg: 'Post not found' })
+      res.json(post)
+   } catch (err) {
+      console.error(err.message)
+      if (err.kind === 'ObjectId')
+         return res.status(404).json({ msg: 'Post not found' })
       res.status(500).send('Server Error')
    }
 })
