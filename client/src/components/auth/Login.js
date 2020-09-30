56f33d73392659856d751c585bf3652cc9d1b1e0
Ralph Pamintuan <pralphnest@gmail.com>
2020-09-30 11:54:56 +0800

    login and register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,46 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
-   return <div>Login</div>
+   const [userData, setUserData] = useState({
+      email: '',
+      password: '',
+   })
+
+   const handler = (e) =>
+      setUserData({ ...userData, [e.target.name]: e.target.value })
+
+   const submitHandler = async (e) => {
+      e.preventDefault()
+      console.log('SUCCESS')
+   }
+
+   const { email, password } = userData
+
+   return (
+      <Fragment>
+         <form onSubmit={(e) => submitHandler(e)}>
+            <input
+               type='email'
+               placeholder='Email Address'
+               name='email'
+               value={email}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <input
+               type='password'
+               placeholder='Password'
+               name='password'
+               value={password}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <input type='submit' value='Login' />
+         </form>
+         <p>
+            Don't have an account? <Link to='/login'>Sign Up</Link>
+         </p>
+      </Fragment>
+   )
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,77 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
-   return <div>Register</div>
+   const [userData, setUserData] = useState({
+      name: '',
+      email: '',
+      password: '',
+      conPassword: '',
+   })
+
+   const handler = (e) =>
+      setUserData({ ...userData, [e.target.name]: e.target.value })
+
+   const submitHandler = async (e) => {
+      e.preventDefault()
+      if (password !== conPassword) console.log('Password do no match')
+      else {
+         console.log('SUCCESS')
+      }
+   }
+
+   const { name, email, password, conPassword } = userData
+
+   return (
+      <Fragment>
+         <h1>Sign Up</h1>
+         <p>
+            <i className='far fa-user'></i>Create account
+         </p>
+         <form onSubmit={(e) => submitHandler(e)}>
+            <input
+               type='text'
+               placeholder='Name'
+               name='name'
+               value={name}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <input
+               type='email'
+               placeholder='Email Address'
+               name='email'
+               value={email}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <small>
+               This site uses Gravatar so if you want a profile image, use a
+               Gravatar email
+            </small>
+            <input
+               type='password'
+               placeholder='Password'
+               minLength='6'
+               name='password'
+               value={password}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <input
+               type='password'
+               placeholder='Confirm Password'
+               minLength='6'
+               name='conPassword'
+               value={conPassword}
+               required
+               onChange={(e) => handler(e)}
+            />
+            <input type='submit' value='Register' />
+         </form>
+         <p>
+            Already have an account? <Link to='/login'>Sign In</Link>
+         </p>
+      </Fragment>
+   )
 }
